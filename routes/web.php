2cb5e1d14d6223e9feb109dd5ff1232d166650fd
Nguyen Tai Duc Anh <ducanhanh234@gmail.com>
2026-06-02 20:57:57 +0700

    'create_layout_tour_favorite+dieu_khoan'

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/danh_sach_tour_yeu_thich', function () {
+    return view('danh_sach_tour_yeu_thich.index');
+});
+Route::get('/dieu_khoan', function () {
+    return view('dieu_khoan.index');
+});

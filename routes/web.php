6c67745110f9547dd96cc0f03c33f2e2b370aac7
Ngô Việt Hùng <ngoviethung481@gmail.com>
2026-06-03 22:21:21 +0700

    tour+chi_tiet_tour

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TourController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/tours', [TourController::class, 'index'])->name('tours.index');
+Route::get('/tours/{tour}', [TourController::class, 'show'])->name('tours.show');

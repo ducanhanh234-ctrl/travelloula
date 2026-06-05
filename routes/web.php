43c95c2ed84e7532defa9efcdaecb0d5376562cb
Hoang Son <sonbe211@gmail.com>
2026-06-05 21:22:58 +0700

    design_layout_bai_viet+chi_tiet_bai_viet

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/bai_viet', function(){
+    return view('bai_viet.index');
+});
+Route::get('/{id}/bai_viet', function(){
+    return view('bai_viet.detail');
+})->name('bai_viet.detail');

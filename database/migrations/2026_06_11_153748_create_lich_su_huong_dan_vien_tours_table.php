08570fc449cc85c5fb09b122fdb8106b99741a59
Nguyen Tai Duc Anh <ducanhanh234@gmail.com>
2026-06-12 22:46:34 +0700

    'fix_csdl3'

--- database/migrations/2026_06_11_153748_create_lich_su_huong_dan_vien_tours_table.php
+++ database/migrations/2026_06_11_153748_create_lich_su_huong_dan_vien_tours_table.php
@@ -9,28 +9,28 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-  public function up(): void
-{
-    Schema::create('lich_su_huong_dan_vien_tours', function (Blueprint $table) {
-        $table->id();
+    public function up(): void
+    {
+        Schema::create('lich_su_huong_dan_vien_tours', function (Blueprint $table) {
+            $table->id();
 
-        $table->foreignId('ma_huong_dan_vien')
-              ->constrained('huong_dan_viens')
-              ->cascadeOnDelete();
+            $table->foreignId('ma_huong_dan_vien')
+                ->constrained('huong_dan_viens')
+                ->cascadeOnDelete();
 
-          $table->foreignId('ma_tour')
-              ->constrained('tour_du_lichs')
-              ->cascadeOnDelete();
+            $table->foreignId('ma_tour')
+                ->constrained('tour_du_lichs')
+                ->cascadeOnDelete();
 
-        $table->date('ngay_bat_dau_tour');
-        $table->date('ngay_ket_thuc_tour');
+            $table->date('ngay_bat_dau_tour');
+            $table->date('ngay_ket_thuc_tour');
 
-        $table->tinyInteger('danh_gia')->nullable();
-        $table->text('phan_hoi')->nullable();
+            $table->tinyInteger('danh_gia')->nullable();
+            $table->text('phan_hoi')->nullable();
 
-        $table->timestamps();
-    });
-}
+            $table->timestamps();
+        });
+    }
     /**
      * Reverse the migrations.
      */

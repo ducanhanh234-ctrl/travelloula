3d9727994030c723b3f32b481b8422a0cac5eecd
TommDeathzZ <lebaduc1709abc@gmail.com>
2026-06-15 11:46:18 +0700

    Test Guide

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\VaiTroController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TourController;
-
+// Client routes
 Route::get('/', function () {
     return view('Client.trang_chu.index');
 });
@@ -44,12 +44,12 @@ Route::post('/register', [AuthController::class, 'register'])->name('register.pe
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.perform');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
+// Login ADMIN
 Route::prefix('Admin')->name('Admin.')->middleware(['auth', \App\Http\Middleware\IsAdmin::class])->group(function () {
     Route::get('/', function () {
         return view('Layouts.admin');
     })->name('dashboard');
-
+// Admin routes
     Route::resource('users', UserController::class);
     // Route::resource('vai-tros', VaiTroController::class);
     // Route::resource('quyen-hans', QuyenHanController::class);
@@ -57,9 +57,11 @@ Route::prefix('Admin')->name('Admin.')->middleware(['auth', \App\Http\Middleware
     Route::resource('huong-dan-viens', HuongDanVienController::class);
 });
 
+// Login Guide
 Route::prefix('Guide')->name('Guide.')->middleware(['auth', \App\Http\Middleware\IsGuide::class])->group(function () {
     Route::get('/', function () {
         return view('Layouts.guide');
     })->name('dashboard');
+//Guide routes
 });
 
